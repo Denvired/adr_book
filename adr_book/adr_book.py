4104933be4be9f9a628e134ea84b77434548fca3
Denvired <dicompufa@gmail.com>
2023-12-07 16:51:48 +0500

    start Address Book project

---
+++ adr_book/adr_book.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Создадим скрипт для адресной книги, сохранение и чтение ее из файла.
+# Возможности: просмотр, добавление, изменение, поиск, удаление записей
+
+
+# тело для вывода меню и дальнейшего выбора
+def menu():
+    print('menu is working')
+# End of menu()
+
+# основное тело скрипта
+def main():
+    print('main working')
+    menu()
+# End of func main():
+
+
+# start main()
+if __name__ == '__main__':
+    main()

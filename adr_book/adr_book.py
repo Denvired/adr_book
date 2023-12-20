7a91f5996f912c28d2b014049204dde282792a97
Denvired <dicompufa@gmail.com>
2023-12-21 00:24:21 +0500

    finished, but have some issues)) my first not small Python script

--- adr_book/adr_book.py
+++ adr_book/adr_book.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # I'm make this script as homework from my book. this is first quest.
 # we save and load data (list of objects) to file
-# possibilities: view, add, edit, find, delete records
-# I can do this script very simple, without one line analising, and using pickle for save and load data,
-# but Im have a new skills using this way
+# possibilities: view, add, edit, find, delete records. save and load to .csv
+# I can do this script very simple, without one line analizing, and using pickle for save and load data,
+# but I haved a new skills by using this way
 # Создадим скрипт для адресной книги, сохранение и чтение ее из файла.
 # Возможности: просмотр, добавление, изменение, поиск, удаление записей
 
